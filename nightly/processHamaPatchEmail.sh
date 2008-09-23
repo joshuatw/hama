f8e044ef4b5d12b8aa00ddce527765b4330ba4b0
Edward J. Yoon <edwardyoon@apache.org>
2008-09-23 03:16:29 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama@698071 13f79535-47bb-0310-9956-ffa450edef68

--- nightly/processHamaPatchEmail.sh
+++ nightly/processHamaPatchEmail.sh
@@ -26,7 +26,9 @@ do
 done
 
 ### If this email indicates a new patch, start a build
-if [[ -n $patch && ! -d $QUEUE_DIR/$defect ]] ; then
+## -n $patch && 
+
+if [[ ! -d $QUEUE_DIR/$defect ]] ; then
   echo "$defect is being processed at `date`" >> $LOG
   mkdir $QUEUE_DIR/$defect
 
