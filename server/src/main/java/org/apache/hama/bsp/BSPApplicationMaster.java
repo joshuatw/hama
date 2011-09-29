8052a07a3c8675f247bac1903bea3e40896c3833
Thomas Jungblut <tjungblut@apache.org>
2011-09-29 17:10:32 +0000

    -Started adding BSPJob Interface and Implementation -Started handling of configurations
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/branches/HamaV2@1177368 13f79535-47bb-0310-9956-ffa450edef68

--- server/src/main/java/org/apache/hama/bsp/BSPApplicationMaster.java
+++ server/src/main/java/org/apache/hama/bsp/BSPApplicationMaster.java
@@ -17,25 +17,67 @@
  */
 package org.apache.hama.bsp;
 
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.ipc.YarnRPC;
 import org.apache.hadoop.yarn.util.Records;
+import org.apache.hama.HamaConfiguration;
 
+/**
+ * BSPApplicationMaster is an application master for Apache Hamas BSP Engine.
+ */
 public class BSPApplicationMaster {
 
-	private Configuration conf;
+	private static final Log LOG = LogFactory
+			.getLog(BSPApplicationMaster.class);
+
+	private Configuration localConf;
+	private Configuration jobConf;
+
+	private FileSystem fs;
+
 	private YarnRPC yarnRPC;
 	private ApplicationId appId;
 
-	private BSPApplicationMaster(String[] args) {
+	private BSPApplicationMaster(String[] args) throws IOException {
+		if (args.length != 1) {
+			throw new IllegalArgumentException();
+		}
+
+		localConf = new YarnConfiguration();
+		jobConf = getSubmitConfiguration(args[0]);
 		appId = Records.newRecord(ApplicationId.class);
-		conf = new Configuration();
-		yarnRPC = YarnRPC.create(conf);
+		yarnRPC = YarnRPC.create(localConf);
+		fs = FileSystem.get(localConf);
+	}
+
+	private Configuration getSubmitConfiguration(String path) {
+		Path jobSubmitPath = new Path(path);
+		Configuration jobConf = new HamaConfiguration();
+		jobConf.addResource(jobSubmitPath);
+		return jobConf;
+	}
+
+	private void start() {
+
 	}
 
 	public static void main(String[] args) {
-		new BSPApplicationMaster(args);
+		// TODO we expect getting the qualified path of the job.xml as the first
+		// element in the arguments
+		try {
+			new BSPApplicationMaster(args).start();
+		} catch (Exception e) {
+			LOG.fatal("Error starting BSPApplicationMaster", e);
+			System.exit(1);
+		}
 	}
 
 }
