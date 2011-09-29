e0a7e99c8e491ae730797d1df01f7d9a8dc79323
Thomas Jungblut <tjungblut@apache.org>
2011-09-29 17:10:32 +0000

    -Started adding BSPJob Interface and Implementation -Started handling of configurations
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama@1177368 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/HamaV2/server/src/main/java/org/apache/hama/bsp/BSPJob.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hama.bsp;
+
+import java.util.Map;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * Main interface to interact with the job. Provides only getters.
+ */
+public interface BSPJob {
+
+	public enum JobState {
+		NEW, RUNNING, SUCCESS, FAILED, KILLED
+	}
+
+	BSPJobID getID();
+
+	String getName();
+
+	JobState getState();
+
+	Map<TaskAttemptID, Task> getTasks();
+
+	Task getTask(TaskAttemptID taskID);
+
+	int getTotalBSPTasks();
+
+	/**
+	 * @return a path to where the config file for this job is located.
+	 */
+	Path getConfFile();
+
+}
